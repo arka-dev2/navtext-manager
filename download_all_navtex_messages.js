5e2d92870e07e43d9fc4586886e5edbe15621f67
Ciro Grazioli <c.grazioli@arka-rnd.it>
2024-11-13 16:30:01 +0100

    rinominato il file per scaricare i navtex

--- download_all_navtex_messages.js
+++ download_all_navtex_messages.js
@@ -1,86 +1,82 @@
+//lo script scarica i navtex solo di oggi
 const puppeteer = require("puppeteer");
 const axios = require("axios");
 const fs = require("fs");
 const messages = [];
-const txtFileLinks = [];
+const linkNavTex = [];
 let page;
 
 (async () => {
   const browser = await puppeteer.launch();
   page = await browser.newPage();
-  await page.goto(
-    "https://www.navtex.net/Navtex_Archive/2024%20Jan-Oct/202401%20/"
-  );
-  await searceFile();
+  await getLinkNavtex();
   await downloadMessages();
   writeMessagesInFile();
 
   await browser.close();
 })();
 
-async function searceFile() {
-  let links = await page.$$("body > table > tbody > tr > td > a");
-  let homelink = links[0];
-  links = links.slice(1, links.length);
-  for (let j = 0; j < links.length; j++) {
-    // if (links[j] === undefined) break;
-    let href = await links[j].getProperty("href");
-    let hrefValue = await href.jsonValue();
-    if (!hrefValue.endsWith(".txt")) {
-      hrefValue = hrefValue.substring(0, hrefValue.length - 1);
-      let cartella = hrefValue
-        .substring(hrefValue.lastIndexOf("/") + 1, hrefValue.length)
-        .replaceAll("%20", " ");
-      console.log(cartella);
-      try {
-        links[j].click();
-      } catch (e) {
-        console.log(e);
-        continue;
-      }
-
-      await page.waitForNavigation();
-      await searceFile();
-      links = await page.$$("body > table > tbody > tr > td > a");
-      homelink = links[0];
-      links = links.slice(1, links.length);
-    } else {
-      txtFileLinks.push(hrefValue);
+async function getLinkNavtex() {
+  // const pageNumber = await getPageNumber();
+  const pageNumber = 40;
+  for (let i = 1; i <= pageNumber; i++) {
+    console.log("pagina numero : " + i);
+    await page.goto(`https://marinesafety.net/?query-52-page=${i}`);
+    let links = await page.$$(
+      "body > div.wp-site-blocks > div > div:nth-child(2) > div > ul > li > div > div:nth-child(3) > h4 > a"
+    );
+    for (let link of links) {
+      let href = await link.getProperty("href");
+      let hrefValue = await href.jsonValue();
+      linkNavTex.push(hrefValue);
     }
   }
-  homelink.click();
-  await page.waitForNavigation();
+}
+
+async function getPageNumber() {
+  await page.goto(`https://marinesafety.net/`);
+  const element = await page.$(
+    "body > div.wp-site-blocks > div > div:nth-child(2) > div > nav > div > a:nth-child(5)"
+  );
+  const innerHTML = await page.evaluate((el) => el.innerHTML, element);
+  const pageNumber = Number(innerHTML.replaceAll(",", "."));
+  return pageNumber;
 }
 
 async function downloadMessages() {
-  console.log(txtFileLinks.length);
-  for (let hrefValue of txtFileLinks) {
-    console.log("download : " + hrefValue);
-    const message = await downloadFileToVariable(hrefValue);
-    messages.push(message);
-  }
-  // for (let i = 0; i < 3000; i++) {
-  //   console.log("download : " + txtFileLinks[i]);
-  //   const message = await downloadFileToVariable(txtFileLinks[i]);
-  //   messages.push(message);
-  // }
-}
-
-async function downloadFileToVariable(fileUrl) {
-  try {
-    const response = await axios.get(fileUrl, { responseType: "arraybuffer" });
-    return response.data.toString("utf-8");
-  } catch (error) {
-    console.error("Errore durante il download:", error);
+  let count = 0;
+  for (let hrefValue of linkNavTex) {
+    console.log(count++);
+    try {
+      await page.goto(hrefValue);
+      const elements = await page.$$(
+        "#wp--skip-link--target > div > div.wp-block-column.is-layout-constrained.wp-block-column-is-layout-constrained > div.vertical-overlap.wp-block-group.has-header-background-color.has-black-background-color.has-text-color.has-background.has-link-color.wp-elements-36612680afc3b1e1f28572441719f0a2.is-layout-constrained.wp-container-core-group-is-layout-6.wp-block-group-is-layout-constrained > div.entry-content.alignfull.wp-block-post-content.is-layout-constrained.wp-block-post-content-is-layout-constrained > p"
+      );
+      let message = "";
+      for (let element of elements) {
+        message += await page.evaluate((el) => el.innerHTML, element);
+      }
+      message = message.replaceAll("<br>", "");
+      message = message.replaceAll("\n", " ");
+      if (message.includes("</div>")) {
+        const start = message.indexOf("<div");
+        const end = message.lastIndexOf("</div>") + 6;
+        message =
+          message.substring(0, start) + message.substring(end, message.length);
+      }
+      messages.push(message);
+    } catch (e) {
+      console.log("errore");
+    }
   }
 }
 
 function writeMessagesInFile(fileUrl) {
   let output = "[\n";
   for (let message of messages) {
-    message = message.replaceAll("\n", " ").replaceAll("\r", "");
-    if (!message.endsWith("NNNN"))
-      message = message.substring(0, message.length - 1);
+    // message = message.replaceAll("\n", " ").replaceAll("\r", "");
+    // if (!message.endsWith("NNNN"))
+    //   message = message.substring(0, message.length - 1);
     output += '\t"' + message + '",\n';
   }
   output = output.substring(0, output.length - 2) + "\n]";
