6c5297ad158fe1ce6fa5fbe917f0b7efb814db53
Ciro Grazioli <c.grazioli@arka-rnd.it>
2024-11-20 12:56:29 +0100

    modificato il messageDAO per gestire l'errore internamente e non nella callback

--- Dao/MessageDAO.js
+++ Dao/MessageDAO.js
@@ -4,38 +4,41 @@ const Message = require("../Entity/message");
 class MessageDAO {
   getAllMessage(callback) {
     const query = "SELECT * FROM messages";
-    conn.query(query, (error, results) => {
-      if (error) return callback(error, null);
-      let messages = [];
-      for (let result of results) {
-        let message = new Message(
-          result.link,
-          result.pubblication_date,
-          result.type,
-          result.text
-        );
-        console.log(message);
-        messages.push(message);
+    conn.query(query, (err, results) => {
+      if (err) console.log(err);
+      else {
+        let messages = [];
+        for (let result of results) {
+          let message = new Message(
+            result.link,
+            result.pubblication_date,
+            result.type,
+            result.text
+          );
+          messages.push(message);
+        }
+        callback(messages);
       }
-      callback(null, messages);
     });
   }
 
   getMessage(id, callback) {
     const query = "SELECT * FROM messages where link = ?";
     const values = [id];
-    conn.query(query, values, (error, result) => {
-      if (error) return callback(error, null);
-      let message = null;
-      if (result.length !== 0) {
-        message = new Message(
-          result[0].link,
-          result[0].publication_date,
-          result[0].type,
-          result[0].text
-        );
+    conn.query(query, values, (err, result) => {
+      if (err) console.log(err);
+      else {
+        let message = null;
+        if (result.length !== 0) {
+          message = new Message(
+            result[0].link,
+            result[0].publication_date,
+            result[0].type,
+            result[0].text
+          );
+        }
+        callback(message);
       }
-      callback(null, message);
     });
   }
 
@@ -48,15 +51,14 @@ class MessageDAO {
       message.type,
       message.text,
     ];
-    conn.query(query, values, (error) => {
-      if (error) return "errore durante l'inserimento nella tabella messages";
+    conn.query(query, values, (err) => {
+      if (err) return "errore durante l'inserimento nella tabella messages";
     });
   }
 
   insertAllMessage(messages) {
     const query =
       "insert into messages (link, publication_date, type, text) values(?,?,?,?)";
-
     for (let message of messages) {
       const values = [
         message.link,
@@ -64,8 +66,8 @@ class MessageDAO {
         message.type,
         message.text,
       ];
-      conn.query(query, values, (error) => {
-        if (error) return "errore durante l'inserimento nella tabella messages";
+      conn.query(query, values, (err) => {
+        if (err) return "errore durante l'inserimento nella tabella messages";
       });
     }
   }
