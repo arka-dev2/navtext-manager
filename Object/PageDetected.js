df75a7fc0a9055d4ef50f020107ff321a0a3db6e
Ciro Grazioli <c.grazioli@arka-rnd.it>
2024-11-25 12:35:10 +0100

    creato lo script per il download dei navtex del giorno

--- Object/PageDetected.js
+++ Object/PageDetected.js
@@ -33,6 +33,39 @@ class PageDetected {
     return pageNumber;
   }
 
+  async getPageNumber2(linkDB) {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const todayStringDate = `${year}-${month}-${day}`;
+
+    let numPage;
+    for (numPage = 1; true; numPage++) {
+      const link = `https://marinesafety.net/?query-52-page=${numPage}`;
+      const dates = await this.#getDateArr(link);
+      if (dates.includes(todayStringDate)) continue;
+      else break;
+    }
+    return numPage - 1;
+  }
+
+  async #getDateArr(pageUrl) {
+    const { data: html } = await axios.get(pageUrl);
+    const $ = cheerio.load(html);
+    const dates = [];
+    const divs = $(
+      "body > div.wp-site-blocks > div > div:nth-child(2) > div > ul > li > div > div:nth-child(1)"
+    );
+
+    $(divs).each((index, div) => {
+      const links = $(div).find("a");
+      dates.push(links.eq(0).text());
+    });
+
+    return dates;
+  }
+
   async dawnloadNavtex(link) {
     const { data: html } = await axios.get(link);
     const $ = cheerio.load(html);
---
+++ download_navtex_messages.js
@@ -0,0 +1,38 @@
+//lo script scarica tutti i navtex dal sito marine safety
+const PageDetected = require("./Object/PageDetected.js");
+const ProgressBar = require("./Object/ProgressBar.js");
+const messageDAO = require("./Dao/MessageDAO.js");
+const conn = require("./conn.js");
+
+(async () => {
+  const linkDB = messageDAO.getAllMessage().map((el) => el.link);
+  const pageDetected = new PageDetected();
+  let pageNumber = await pageDetected.getPageNumber2(linkDB);
+  const progressBar = new ProgressBar(
+    pageNumber,
+    "pagine",
+    "download dei navtex"
+  );
+
+  for (let i = 1; i <= pageNumber; i++) {
+    try {
+      const link = `https://marinesafety.net/?query-52-page=${i}`;
+      const navtexObjArr = await pageDetected.getNavtexObjArr(link);
+      for (let navtexObj of navtexObjArr) {
+        if (!linkDB.includes(navtexObj.link)) {
+          const link = navtexObj.link;
+          const publicationDate = navtexObj.date;
+          const type = navtexObj.type;
+          const text = await pageDetected.dawnloadNavtex(navtexObj.link);
+          const message = { link, publicationDate, type, text };
+          messageDAO.insertMessage(message);
+        }
+      }
+    } catch (err) {
+      console.error("Errore nel caricamento della pagina:", err.message);
+    }
+    progressBar.updatedOneStep();
+  }
+  progressBar.complete();
+  conn.dispose();
+})();
