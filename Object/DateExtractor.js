96ca3c7da5caa7b27ed3dab3e3b8304e27fe514e
Ciro Grazioli <c.grazioli@arka-rnd.it>
2024-11-28 12:08:59 +0100

    gestite tutte le estrapolazioni con le regex

--- Object/DateExtractor.js
+++ Object/DateExtractor.js
@@ -1,10 +1,10 @@
 class DateExtractor {
   constructor() {
     this.regexs = [
-      /\b\d{6}Z (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) \d{2}\b/g,
-      /\b\d{2} (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) \d{2} TO \d{2} (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) \d{2}\b/g,
-      /\b\d{6} UTC (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) \d{2}\b/g,
-      /\b\d{2} (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) \d{2} from \d{4}UTC to \d{4}UTC\b/g,
+      /\b(\d{2})(\d{2})(\d{2})Z (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2})\b/g,
+      /\b(\d{2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2}) TO (\d{2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2})\b/g,
+      /\b(\d{2})(\d{2})(\d{2}) UTC (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2})\b/g,
+      /\b(\d{2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2}) from (\d{4})UTC to (\d{4})UTC\b/g,
       /\b(\d{2}) (JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER) (\d{2}) from (\d{4})UTC to (\d{4})UTC\b/g,
     ];
   }
@@ -22,77 +22,89 @@ class DateExtractor {
 
   //gestione di questo formato : 062323Z SEP 24
   getDateFormat1(dateString) {
-    let day = dateString.substring(0, 2);
-    let hour = Number(dateString.substring(2, 4));
-    let minute = Number(dateString.substring(4, 6));
-    let month = this.#getMonth1(dateString.substring(8, 11));
-    let year = `20${dateString.substring(12, 14)}`;
+    let output = dateString.replace(
+      this.regexs[0],
+      (match, day, hour, minute, month, year) => {
+        hour = Number(hour);
+        minute = Number(minute);
 
-    const desc = hour > 12 ? "PM" : "AM";
-    hour = hour > 12 ? `${hour - 12}` : hour;
-    hour = hour < 10 ? `0${hour}` : hour;
-    minute = minute < 10 ? `0${minute}` : minute;
+        const desc = hour > 12 ? "PM" : "AM";
+        hour = hour > 12 ? `${hour - 12}` : hour;
+        hour = hour < 10 ? `0${hour}` : hour;
+        minute = minute < 10 ? `0${minute}` : minute;
 
-    return {
-      date: `${day}/${month}/${year}`,
-      time: `${hour}:${minute} ${desc}`,
-    };
+        const date = `${day}/${this.#getMonth1(month)}/20${year}`;
+        const time = `${hour}:${minute} ${desc}`;
+        return `${date}-${time}`;
+      }
+    );
+    output = output.split("-");
+    return { date: output[0], time: output[1] };
   }
 
   //gestione di questo formato : 17 NOV 24 TO 02 DEC 24
   getDateFormat2(dateString) {
-    dateString = dateString.split(" TO ")[0];
-    let day = dateString.substring(0, 2);
-    let month = this.#getMonth1(dateString.substring(3, 6));
-    let year = `20${dateString.substring(7, 9)}`;
-    return {
-      date: `${day}/${month}/${year}`,
-      time: "00:00 AM",
-    };
+    let output = dateString.replace(
+      this.regexs[1],
+      (match, dayFrom, monthFrom, yearFrom, dayTo, monthTo, yearTo) => {
+        const date = `${dayFrom}/${this.#getMonth1(monthFrom)}/20${yearFrom}`;
+        const time = `00:00 AM`;
+        return `${date}-${time}`;
+      }
+    );
+    output = output.split("-");
+    return { date: output[0], time: output[1] };
   }
 
   //gestione di questo formato : 192030 UTC APR 25
   getDateFormat3(dateString) {
-    let day = dateString.substring(0, 2);
-    let hour = Number(dateString.substring(2, 4));
-    let minute = Number(dateString.substring(4, 6));
-    let month = this.#getMonth1(dateString.substring(11, 14));
-    let year = `20${dateString.substring(15, 17)}`;
+    let output = dateString.replace(
+      this.regexs[2],
+      (match, day, hour, minute, month, year) => {
+        hour = Number(hour);
+        minute = Number(minute);
 
-    const desc = hour > 12 ? "PM" : "AM";
-    hour = hour > 12 ? `${hour - 12}` : hour;
-    hour = hour < 10 ? `0${hour}` : hour;
-    minute = minute < 10 ? `0${minute}` : minute;
+        const desc = hour > 12 ? "PM" : "AM";
+        hour = hour > 12 ? `${hour - 12}` : hour;
+        hour = hour < 10 ? `0${hour}` : hour;
+        minute = minute < 10 ? `0${minute}` : minute;
 
-    return {
-      date: `${day}/${month}/${year}`,
-      time: `${hour}:${minute} ${desc}`,
-    };
+        const date = `${day}/${this.#getMonth1(month)}/20${year}`;
+        const time = `${hour}:${minute} ${desc}`;
+        return `${date}-${time}`;
+      }
+    );
+    output = output.split("-");
+    return { date: output[0], time: output[1] };
   }
 
   //gestione di questo formato : 31 OCT 24 from 0300UTC to 1600UTC
   getDateFormat4(dateString) {
-    let day = dateString.substring(0, 2);
-    let hour = Number(dateString.substring(15, 17));
-    let minute = Number(dateString.substring(17, 19));
-    let month = this.#getMonth1(dateString.substring(3, 6));
-    let year = `20${dateString.substring(7, 9)}`;
+    console.log(dateString);
+    let output = dateString.replace(
+      this.regexs[3],
+      (match, day, month, year, from, to) => {
+        let hour = Number(from.substring(0, 2));
+        let minute = Number(from.substring(2, 4));
 
-    const desc = hour > 12 ? "PM" : "AM";
-    hour = hour > 12 ? `${hour - 12}` : hour;
-    hour = hour < 10 ? `0${hour}` : hour;
-    minute = minute < 10 ? `0${minute}` : minute;
+        const desc = hour > 12 ? "PM" : "AM";
+        hour = hour > 12 ? `${hour - 12}` : hour;
+        hour = hour < 10 ? `0${hour}` : hour;
+        minute = minute < 10 ? `0${minute}` : minute;
 
-    return {
-      date: `${day}/${month}/${year}`,
-      time: `${hour}:${minute} ${desc}`,
-    };
+        const date = `${day}/${this.#getMonth2(month)}/20${year}`;
+        const time = `${hour}:${minute} ${desc}`;
+        return `${date}-${time}`;
+      }
+    );
+    output = output.split("-");
+    return { date: output[0], time: output[1] };
   }
 
   //gestione di questo formato : 05 SEPTEMBER 24 from 1100UTC to 2300UTC
   getDateFormat5(dateString) {
     let output = dateString.replace(
-      this.regexs[5],
+      this.regexs[4],
       (match, day, month, year, from, to) => {
         let hour = Number(from.substring(0, 2));
         let minute = Number(from.substring(2, 4));
