f1a1bf7e07d5daa496f237464a74d964cade8d9c
Ciro Grazioli <c.grazioli@arka-rnd.it>
2024-11-28 10:12:49 +0100

    modificato l'oggetto dateExtractor

--- Object/DateExtractor.js
+++ Object/DateExtractor.js
@@ -12,23 +12,22 @@ class DateExtractor {
 
   getDate(string) {
     let dateExtract = string.match(this.regex1);
-    if (dateExtract !== null) return this.getDateFormat1(dateExtract);
+    if (dateExtract !== null) return this.getDateFormat1(dateExtract[0]);
 
     dateExtract = string.match(this.regex2);
-    if (dateExtract !== null) return this.getDateFormat2(dateExtract);
+    if (dateExtract !== null) return this.getDateFormat2(dateExtract[0]);
 
     dateExtract = string.match(this.regex3);
-    if (dateExtract !== null) return this.getDateFormat3(dateExtract);
+    if (dateExtract !== null) return this.getDateFormat3(dateExtract[0]);
 
     dateExtract = string.match(this.regex4);
-    if (dateExtract !== null) return this.getDateFormat4(dateExtract);
+    if (dateExtract !== null) return this.getDateFormat4(dateExtract[0]);
 
     return { date: null, time: null };
   }
 
   //gestione di questo formato : 062323Z SEP 24
-  getDateFormat1(dateExtract) {
-    const dateString = dateExtract[0];
+  getDateFormat1(dateString) {
     let day = dateString.substring(0, 2);
     let hour = Number(dateString.substring(2, 4));
     let minute = Number(dateString.substring(4, 6));
@@ -47,8 +46,8 @@ class DateExtractor {
   }
 
   //gestione di questo formato : 17 NOV 24 TO 02 DEC 24
-  getDateFormat2(dateExtract) {
-    let dateString = dateExtract[0].split(" TO ")[0];
+  getDateFormat2(dateString) {
+    dateString = dateString.split(" TO ")[0];
     let day = dateString.substring(0, 2);
     let month = this.#getMonth(dateString.substring(3, 6));
     let year = `20${dateString.substring(7, 9)}`;
@@ -59,8 +58,7 @@ class DateExtractor {
   }
 
   //gestione di questo formato : 192030 UTC APR 25
-  getDateFormat3(dateExtract) {
-    const dateString = dateExtract[0];
+  getDateFormat3(dateString) {
     let day = dateString.substring(0, 2);
     let hour = Number(dateString.substring(2, 4));
     let minute = Number(dateString.substring(4, 6));
@@ -79,8 +77,7 @@ class DateExtractor {
   }
 
   //gestione di questo formato : 31 OCT 24 from 0300UTC to 1600UTC
-  getDateFormat4(dateExtract) {
-    const dateString = dateExtract[0];
+  getDateFormat4(dateString) {
     let day = dateString.substring(0, 2);
     let hour = Number(dateString.substring(15, 17));
     let minute = Number(dateString.substring(17, 19));
