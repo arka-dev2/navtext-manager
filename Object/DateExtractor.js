36a27f66fabcbe5aa9f038ad8c5af1d6f6649281
Ciro Grazioli <c.grazioli@arka-rnd.it>
2024-11-29 09:59:01 +0100

    cambiata la struttura de dateExtractor

--- Object/DateExtractor.js
+++ Object/DateExtractor.js
@@ -1,149 +1,121 @@
 class DateExtractor {
   constructor() {
-    this.regexs = [
-      /\b(\d{2})(\d{2})(\d{2})Z (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2})\b/g,
-      /\b(\d{1,2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2}) TO (\d{1,2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2})\b/g,
-      /\b(\d{2})(\d{2})(\d{2}) UTC (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2})\b/g,
-      /\b(\d{1,2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2}) from (\d{4})UTC to (\d{4})UTC\b/g,
-      /\b(\d{1,2}) (JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER) (\d{2}) from (\d{4})UTC to (\d{4})UTC\b/g,
-      /\b(\d{1,2})\s+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+(\d{4})\s+(?:TIME|time):\s+(\d{4})\s+UTC/g,
+    this.arr = [
+      //gestione di questo formato : 062323Z SEP 24
+      {
+        regex:
+          /\b(\d{2})(\d{2})(\d{2})Z (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2})\b/g,
+        callback: (match, day, hour, minute, month, year) => {
+          hour = Number(hour);
+          minute = Number(minute);
+
+          const desc = hour > 12 ? "PM" : "AM";
+          hour = hour > 12 ? `${hour - 12}` : hour;
+          hour = hour < 10 ? `0${hour}` : hour;
+          minute = minute < 10 ? `0${minute}` : minute;
+
+          const date = `${day}/${this.#getMonth1(month)}/20${year}`;
+          const time = `${hour}:${minute} ${desc}`;
+          return `${date}-${time}`;
+        },
+      },
+      //gestione di questo formato : 17 NOV 24 TO 02 DEC 24
+      {
+        regex:
+          /\b(\d{1,2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2}) TO (\d{1,2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2})\b/g,
+        callback: (m, dayFrom, monthFrom, yearFrom, dayTo, monthTo, yearTo) => {
+          const date = `${dayFrom}/${this.#getMonth1(monthFrom)}/20${yearFrom}`;
+          const time = `00:00 AM`;
+          return `${date}-${time}`;
+        },
+      },
+      //gestione di questo formato : 192030 UTC APR 25
+      {
+        regex:
+          /\b(\d{2})(\d{2})(\d{2}) UTC (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2})\b/g,
+        callback: (match, day, hour, minute, month, year) => {
+          hour = Number(hour);
+          minute = Number(minute);
+
+          const desc = hour > 12 ? "PM" : "AM";
+          hour = hour > 12 ? `${hour - 12}` : hour;
+          hour = hour < 10 ? `0${hour}` : hour;
+          minute = minute < 10 ? `0${minute}` : minute;
+
+          const date = `${day}/${this.#getMonth1(month)}/20${year}`;
+          const time = `${hour}:${minute} ${desc}`;
+          return `${date}-${time}`;
+        },
+      },
+      //gestione di questo formato : 31 OCT 24 from 0300UTC to 1600UTC
+      {
+        regex:
+          /\b(\d{1,2}) (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) (\d{2}) from (\d{4})UTC to (\d{4})UTC\b/g,
+        callback: (match, day, month, year, from, to) => {
+          let hour = Number(from.substring(0, 2));
+          let minute = Number(from.substring(2, 4));
+
+          const desc = hour > 12 ? "PM" : "AM";
+          hour = hour > 12 ? `${hour - 12}` : hour;
+          hour = hour < 10 ? `0${hour}` : hour;
+          minute = minute < 10 ? `0${minute}` : minute;
+
+          const date = `${day}/${this.#getMonth1(month)}/20${year}`;
+          const time = `${hour}:${minute} ${desc}`;
+          return `${date}-${time}`;
+        },
+      },
+      //gestione di questo formato : 05 SEPTEMBER 24 from 1100UTC to 2300UTC
+      {
+        regex:
+          /\b(\d{1,2}) (JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER) (\d{2}) from (\d{4})UTC to (\d{4})UTC\b/g,
+        callback: (match, day, month, year, from, to) => {
+          let hour = Number(from.substring(0, 2));
+          let minute = Number(from.substring(2, 4));
+
+          const desc = hour > 12 ? "PM" : "AM";
+          hour = hour > 12 ? `${hour - 12}` : hour;
+          hour = hour < 10 ? `0${hour}` : hour;
+          minute = minute < 10 ? `0${minute}` : minute;
+
+          const date = `${day}/${this.#getMonth2(month)}/20${year}`;
+          const time = `${hour}:${minute} ${desc}`;
+          return `${date}-${time}`;
+        },
+      },
+      //gestione di questo formato : 21 Nov 2024 TIME: 0242 UTC
+      {
+        regex:
+          /\b(\d{1,2})\s+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+(\d{4})\s+(?:TIME|time):\s+(\d{4})\s+UTC/g,
+        callback: (match, day, hour, minute, month, year) => {
+          console.log(time);
+          let hour = Number(time.substring(0, 2));
+          let minute = Number(time.substring(2, 4));
+
+          const desc = hour > 12 ? "PM" : "AM";
+          hour = hour > 12 ? `${hour - 12}` : hour;
+          hour = hour < 10 ? `0${hour}` : hour;
+          minute = minute < 10 ? `0${minute}` : minute;
+
+          const date = `${day}/${this.#getMonth1(month)}/${year}`;
+          const timeSupp = `${hour}:${minute} ${desc}`;
+          return `${date}-${timeSupp}`;
+        },
+      },
     ];
   }
 
   getDate(string) {
-    let count = 1;
-    for (let regex of this.regexs) {
-      let dateExtract = string.match(regex);
-      if (dateExtract !== null)
-        return this["getDateFormat" + count](dateExtract[0]);
-      count++;
+    for (let obj of this.arr) {
+      let dateExtract = string.match(obj.regex);
+      if (dateExtract !== null) {
+        const dateString = dateExtract[0];
+        let output = dateString.replace(obj.regex, obj.callback);
+        output = output.split("-");
+        return { date: output[0], time: output[1] };
+      }
     }
     return { date: null, time: null };
-  }
-
-  //gestione di questo formato : 062323Z SEP 24
-  getDateFormat1(dateString) {
-    let output = dateString.replace(
-      this.regexs[0],
-      (match, day, hour, minute, month, year) => {
-        hour = Number(hour);
-        minute = Number(minute);
-
-        const desc = hour > 12 ? "PM" : "AM";
-        hour = hour > 12 ? `${hour - 12}` : hour;
-        hour = hour < 10 ? `0${hour}` : hour;
-        minute = minute < 10 ? `0${minute}` : minute;
-
-        const date = `${day}/${this.#getMonth1(month)}/20${year}`;
-        const time = `${hour}:${minute} ${desc}`;
-        return `${date}-${time}`;
-      }
-    );
-    output = output.split("-");
-    return { date: output[0], time: output[1] };
-  }
-
-  //gestione di questo formato : 17 NOV 24 TO 02 DEC 24
-  getDateFormat2(dateString) {
-    let output = dateString.replace(
-      this.regexs[1],
-      (match, dayFrom, monthFrom, yearFrom, dayTo, monthTo, yearTo) => {
-        const date = `${dayFrom}/${this.#getMonth1(monthFrom)}/20${yearFrom}`;
-        const time = `00:00 AM`;
-        return `${date}-${time}`;
-      }
-    );
-    output = output.split("-");
-    return { date: output[0], time: output[1] };
-  }
-
-  //gestione di questo formato : 192030 UTC APR 25
-  getDateFormat3(dateString) {
-    let output = dateString.replace(
-      this.regexs[2],
-      (match, day, hour, minute, month, year) => {
-        hour = Number(hour);
-        minute = Number(minute);
-
-        const desc = hour > 12 ? "PM" : "AM";
-        hour = hour > 12 ? `${hour - 12}` : hour;
-        hour = hour < 10 ? `0${hour}` : hour;
-        minute = minute < 10 ? `0${minute}` : minute;
-
-        const date = `${day}/${this.#getMonth1(month)}/20${year}`;
-        const time = `${hour}:${minute} ${desc}`;
-        return `${date}-${time}`;
-      }
-    );
-    output = output.split("-");
-    return { date: output[0], time: output[1] };
-  }
-
-  //gestione di questo formato : 31 OCT 24 from 0300UTC to 1600UTC
-  getDateFormat4(dateString) {
-    let output = dateString.replace(
-      this.regexs[3],
-      (match, day, month, year, from, to) => {
-        let hour = Number(from.substring(0, 2));
-        let minute = Number(from.substring(2, 4));
-
-        const desc = hour > 12 ? "PM" : "AM";
-        hour = hour > 12 ? `${hour - 12}` : hour;
-        hour = hour < 10 ? `0${hour}` : hour;
-        minute = minute < 10 ? `0${minute}` : minute;
-
-        const date = `${day}/${this.#getMonth1(month)}/20${year}`;
-        const time = `${hour}:${minute} ${desc}`;
-        return `${date}-${time}`;
-      }
-    );
-    output = output.split("-");
-    return { date: output[0], time: output[1] };
-  }
-
-  //gestione di questo formato : 05 SEPTEMBER 24 from 1100UTC to 2300UTC
-  getDateFormat5(dateString) {
-    let output = dateString.replace(
-      this.regexs[4],
-      (match, day, month, year, from, to) => {
-        let hour = Number(from.substring(0, 2));
-        let minute = Number(from.substring(2, 4));
-
-        const desc = hour > 12 ? "PM" : "AM";
-        hour = hour > 12 ? `${hour - 12}` : hour;
-        hour = hour < 10 ? `0${hour}` : hour;
-        minute = minute < 10 ? `0${minute}` : minute;
-
-        const date = `${day}/${this.#getMonth2(month)}/20${year}`;
-        const time = `${hour}:${minute} ${desc}`;
-        return `${date}-${time}`;
-      }
-    );
-    output = output.split("-");
-    return { date: output[0], time: output[1] };
-  }
-
-  //gestione di questo formato : 21 Nov 2024 TIME: 0242 UTC
-  getDateFormat6(dateString) {
-    let output = dateString.replace(
-      this.regexs[5],
-      (match, day, month, year, time) => {
-        console.log(time);
-        let hour = Number(time.substring(0, 2));
-        let minute = Number(time.substring(2, 4));
-
-        const desc = hour > 12 ? "PM" : "AM";
-        hour = hour > 12 ? `${hour - 12}` : hour;
-        hour = hour < 10 ? `0${hour}` : hour;
-        minute = minute < 10 ? `0${minute}` : minute;
-
-        const date = `${day}/${this.#getMonth1(month)}/${year}`;
-        const timeSupp = `${hour}:${minute} ${desc}`;
-        return `${date}-${timeSupp}`;
-      }
-    );
-    output = output.split("-");
-    return { date: output[0], time: output[1] };
   }
 
   #getMonth1(stringMonth) {
