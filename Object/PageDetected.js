34a32094c67f69ebd3aa4610df592e1da4832217
Ciro Grazioli <c.grazioli@arka-rnd.it>
2024-12-10 08:45:03 +0100

    aggiunto un nuovo caso per controllare la navarea nei messaggi

--- Object/PageDetected.js
+++ Object/PageDetected.js
@@ -96,13 +96,61 @@ class PageDetected {
     let navarea = "";
     let reference = "";
     const navareaList = text.match(
-      /\bNAVAREA (I|II|III|IV|V|VI|VII|VIII|IX|X|XI|XII|XIII|XIV|XV|XVI|XVII|XVIII|XIX|XX|XXI)(?:\s+(\d{1,5}\/\d{1,4}))?\b/
+      /\b(?:NAVAREA|METAREA) (I|II|III|IV|V|VI|VII|VIII|IX|X|XI|XII|XIII|XIV|XV|XVI|XVII|XVIII|XIX|XX|XXI|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21)(?:\s+(\d{1,5}\/\d{1,4}))?\b/
     );
     if (navareaList !== null) {
-      navarea = navareaList[1];
+      const number = Number(navareaList[1]);
+      navarea = isNaN(number) ? navareaList[1] : this.convertNavare(number);
       reference = navareaList[2] ? navareaList[2] : "";
     }
     return { navarea, reference };
+  }
+
+  convertNavare(number) {
+    switch (number) {
+      case 1:
+        return "I";
+      case 2:
+        return "II";
+      case 3:
+        return "III";
+      case 4:
+        return "IV";
+      case 5:
+        return "V";
+      case 6:
+        return "VI";
+      case 7:
+        return "VII";
+      case 8:
+        return "VIII";
+      case 9:
+        return "IX";
+      case 10:
+        return "X";
+      case 11:
+        return "XI";
+      case 12:
+        return "XII";
+      case 13:
+        return "XIII";
+      case 14:
+        return "XIV";
+      case 15:
+        return "XV";
+      case 16:
+        return "XVI";
+      case 17:
+        return "XVII";
+      case 18:
+        return "XVIII";
+      case 19:
+        return "XIX";
+      case 20:
+        return "XX";
+      case 21:
+        return "XXI";
+    }
   }
 }
 
