915ece93321b67e1d5884a801a3a78c32e4d2b64
Ciro Grazioli <c.grazioli@arka-rnd.it>
2024-12-13 16:01:50 +0100

    tolata la funzione per i messaggi weather e messe funzioni per pulire il testo del messaggio di parti bloccanti e una funzione per controllare se ci sono dei submessages

--- Object/ReportManager.js
+++ Object/ReportManager.js
@@ -18,12 +18,13 @@ class ReportManager {
   }
 
   getReportFromMessage(message) {
-    if (message.type === "WEATHER") return this.getWeatherRportType(message);
+    //----    check per constrollare se nel testo ci sono più navtex    ----
+    this.clearTextMessage(message);
+    const subMessages = this.getMultiNavtexMessages(message.text);
 
-    return { messageType: "uncategorized" };
-  }
+    if (subMessages.length == 1) {
+    }
 
-  getWeatherRportType(message) {
     return {
       messageType: "uncategorized",
       areatype: null,
@@ -34,6 +35,47 @@ class ReportManager {
         description: message.description,
       },
     };
+  }
+
+  getAreaType(text) {
+    if (text.toUpperCase().includes("IN AREA BOUND BY")) return "polygon";
+    if (text.toUpperCase().includes("IN AREAS BOUND BY"))
+      return "multi-polygon";
+    return "";
+  }
+
+  getMultiNavtexMessages(text) {
+    const messages = [];
+    const navareaList = text.match(
+      /\b(?:NAVAREA|METAREA) (I|II|III|IV|V|VI|VII|VIII|IX|X|XI|XII|XIII|XIV|XV|XVI|XVII|XVIII|XIX|XX|XXI|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21)(?:\s+(\d{1,5}\/\d{1,4}))?\b/g
+      // /\bNAVAREA (I|II|III|IV|V|VI|VII|VIII|IX|X|XI|XII|XIII|XIV|XV|XVI|XVII|XVIII|XIX|XX|XXI)(?:\s+(\d{1,5}\/\d{1,4}))?\n\b/g
+    );
+    for (let i = 0; i < navareaList.length - 1; i++) {
+      messages.push(
+        text.substring(
+          text.indexOf(navareaList[i]),
+          text.indexOf(navareaList[i + 1])
+        )
+      );
+    }
+    messages.push(
+      text.substring(
+        text.indexOf(navareaList[navareaList.length - 1]),
+        text.length
+      )
+    );
+    return messages;
+  }
+
+  //questa funzione elimina testo che potrebbe falsare la lettura del messaggio
+  clearTextMessage(message) {
+    let navareaList = message.text.match(
+      /\bCANCEL NAVAREA (I|II|III|IV|V|VI|VII|VIII|IX|X|XI|XII|XIII|XIV|XV|XVI|XVII|XVIII|XIX|XX|XXI)(?:\s+(\d{1,5}\/\d{1,4}))\b/g
+    );
+    if (navareaList !== null) {
+      for (let navarea of navareaList)
+        message.text = message.text.replaceAll(navarea, "");
+    }
   }
 }
 
