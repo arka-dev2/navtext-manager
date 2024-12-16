cca4ab4c0f5cc1e7d9d701de9f5686cdaca61b7e
Ciro Grazioli <c.grazioli@arka-rnd.it>
2024-12-16 11:11:40 +0100

    cambiata il formato dell'output nelle callback del coordinatesExtractor

--- Object/CoordinatesExtractor.js
+++ Object/CoordinatesExtractor.js
@@ -6,7 +6,7 @@ class CoordinatesExtractor {
         regex:
           /(\d{1,3})(?:\s)?(?:–|-)?(?:\s)?(\d{1,3}(?:\.\d{1,3})?)(?:\s)?([NSEW])\s+(\d{1,3})(?:\s)?(?:–|-)?(?:\s)?(\d{1,3})(?:\.\d{1,3})?(?:\s)?([NSEW])/g,
         callback: (m, latDeg, latMin, latDir, lonDeg, lonMin, lonDir) => {
-          return `${latDeg} ${latMin} ${latDir}-${lonDeg} ${lonMin} ${lonDir}`;
+          return `${latDeg}-${latMin}${latDir} ${lonDeg}-${lonMin}${lonDir}`;
         },
       },
       //gestione di questo formato : 05o?=27’24?N , 052o?=40’00?W
@@ -24,7 +24,7 @@ class CoordinatesExtractor {
           lonDec,
           lonDir
         ) => {
-          return `${latDeg} ${latMin}.${latDec} ${latDir}-${lonDeg} ${lonMin}.${lonDec} ${lonDir}`;
+          return `${latDeg}-${latMin}.${latDec}${latDir} ${lonDeg}-${lonMin}.${lonDec}${lonDir}`;
         },
       },
       //gestione di questo formato : 05o?=33,59N 052o?=33,45W
@@ -32,21 +32,21 @@ class CoordinatesExtractor {
         regex:
           /(\d{1,3})o\?=(\d{1,3}(?:,\d{1,3})?)([NSEW])\s(?:–\s+)?(\d{1,3})o\?=(\d{1,3}(?:,\d{1,3})?)([NSEW])/g, //-\s(\d{1,3})o\?
         callback: (match, latDeg, latMin, latDir, lonDeg, lonMin, lonDir) => {
-          return `${latDeg} ${latMin} ${latDir}-${lonDeg} ${lonMin} ${lonDir}`;
+          return `${latDeg}-${latMin}${latDir} ${lonDeg}-${lonMin}${lonDir}`;
         },
       },
       //gestione di questo formato : 6937N 5651W
       {
         regex: /(\d{1,3})(\d{2})([NSEW])\s+(\d{1,3})(\d{2})([NSEW])/g,
         callback: (match, latDeg, latMin, latDir, lonDeg, lonMin, lonDir) => {
-          return `${latDeg} ${latMin} ${latDir}-${lonDeg} ${lonMin} ${lonDir}`;
+          return `${latDeg}-${latMin}${latDir} ${lonDeg}-${lonMin}${lonDir}`;
         },
       },
       //gestione di questo formato : 50N12W
       {
         regex: /(\d{1,3})([NSEW])(?:\s+)?(\d{1,3})([NSEW])/g,
         callback: (match, latDeg, latDir, lonDeg, lonDir) => {
-          return `${latDeg} 00.00 ${latDir}-${lonDeg} 00.00 ${lonDir}`;
+          return `${latDeg}-00.00${latDir} ${lonDeg}-00.00${lonDir}`;
         },
       },
     ];
@@ -60,10 +60,10 @@ class CoordinatesExtractor {
 
         for (let coordinate of coordinateExtract) {
           let output = coordinate.replace(obj.regex, obj.callback);
-          output = output.split("-");
-          const latitude = this.#getAngle(output[0]);
-          const longitude = this.#getAngle(output[1]);
-          coordinatesList.push({ latitude, longitude });
+          output = output.split(" ");
+          const lat = this.#getAngle(output[0]);
+          const long = this.#getAngle(output[1]);
+          coordinatesList.push({ lat, long });
         }
 
         return coordinatesList;
@@ -73,12 +73,15 @@ class CoordinatesExtractor {
     return null;
   }
 
-  // il formato di input che si aspetta é : 10 05.08 N
+  // il formato di input che si aspetta é : 10-05.08N
   #getAngle(angleStr) {
-    const supp = angleStr.split(" ");
-    let direction = supp[2] === "S" || supp[2] === "W" ? -1 : 1;
+    const supp = angleStr.split("-");
+    const latDeg = supp[0];
+    const latMin = supp[1].substring(0, supp[1].length - 1);
+    const directionStr = supp[1].substring(supp[1].length - 1, supp[1].length);
+    let direction = directionStr === "S" || directionStr === "W" ? -1 : 1;
     const angle =
-      direction * (Number(supp[0]) + Number(supp[1].replace(",", ".")) / 60);
+      direction * (Number(latDeg) + Number(latMin.replace(",", ".")) / 60);
     return angle;
   }
 }
